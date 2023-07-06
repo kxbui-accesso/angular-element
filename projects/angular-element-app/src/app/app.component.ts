9252caf632ba5c0b24d88307f436d73b8ed8f627
Khoi Bui <khoi.bui@accesso.com>
2023-07-06 09:37:48 -0400

    add shadow dom

--- projects/angular-element-app/src/app/app.component.ts
+++ projects/angular-element-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['../styles.scss', './app.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class AppComponent {
   constructor(private router: Router) {
